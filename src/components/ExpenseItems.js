ed04ef732edb262f8502ec74dd23178ab7d644c4
33ronak <08arnab98@gmail.com>
2024-10-15 18:25:04 +0530

    Components refactored

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -2,12 +2,13 @@ import "./ExpenseItems.css"
 
 function ExpenseItems() {
     const expenseDate  = new Date(2023, 7,15).toISOString();
-    const expenseTitle = "Book";
-    const expenseAmount = 10;
+    const expenseTitle = "Insuarance";
+    const expenseAmount = 50;
+    const expenseLocation = "Bangalore"
     return (
         <div className = "expense-item">
             <div>{expenseDate}</div>
-            <div className="expense-item__location">Delhi</div>
+            <div className="expense-item__location">{expenseLocation}</div>
             <div className="expense-item__description" >
                 <h2>{expenseTitle}</h2>
                 <div className="expense-item__price" >{expenseAmount}$</div>
