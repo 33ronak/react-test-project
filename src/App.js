474ecd60c8b66c35bd3150d3d1c4ce83eb9f21b9
33ronak <08arnab98@gmail.com>
2024-10-18 08:43:05 +0530

    Form submision handled by lifting state up.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
-    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100, location : "Bangalore"},
-    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10, location : "Delhi" },
-    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1, location : "Hyderabad" },
-    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200, location : "Mumbai" },
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100, location: "Bangalore" },
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10, location: "Delhi" },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1, location: "Hyderabad" },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200, location: "Mumbai" },
   ];
+
+  const saveExpense = (expenseData) => {
+    console.log(expenseData);
+  };
 
   return (
     <div>
-      <NewExpense/>
-      <Expenses expenses = {expenses}/>
+      <NewExpense onSaveExpense={saveExpense} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
