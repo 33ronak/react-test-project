d31cdce09ac548ac173b74c9f230d2873cce4cb0
33ronak <08arnab98@gmail.com>
2024-10-22 07:30:16 +0530

    Filter component refactored

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,41 +1,47 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css"
+import "./Expenses.css";
 import Card from "../UI/Card.js";
 import ExpensesFilter from "./ExpensesFilter.js";
 
 const Expenses = (props) => {
-
   const [filteredYear, setFilteredYear] = useState("2023");
 
   const changeFilterHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);    
-  }
+    setFilteredYear(selectedYear);
+  };
 
   const filteredExpenses = props.expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <>
+        <ExpenseItem
+          key={expense.id}
+          date={expense.date}
+          title={expense.title}
+          price={expense.price}
+          location={expense.location}
+        />
+        {filteredExpenses.length === 1 && <p>Only one expense here. Please add more</p>}
+      </>
+    ));
+  }
 
   return (
-    <Card className = "expenses">
-      <ExpensesFilter 
-      selected ={filteredYear} 
-      onChangeFilter={changeFilterHandler} />
-      {
-        filteredExpenses.map((expense, i) =>{
-          return (
-            <ExpenseItem 
-            key = {expense.id}
-            date = {expense.date} 
-            title = {expense.title} 
-            price = {expense.price} 
-            location = {expense.location}
-            />
-          );
-        })
-      }
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {expensesContent}
     </Card>
   );
-} 
 
-export default Expenses; 
+};
+
+export default Expenses;
